531a407a49f38ec76b43d1005ed9416e6e3e8770
Hayley Bech <hayleybech@gmail.com>
2026-05-08 13:22:08 +0800

    Re-organise roughly by feature

--- features/event_log/EventLog.gd
+++ features/event_log/EventLog.gd
@@ -11,7 +11,7 @@ func add_item(type: EventManager.EventType, content: String) -> void:
 	item.time = TimeManager.get_time_display()
 	item.type = type
 	item.content = content
-	$VBoxContainer.add_child(item)
+	#$VBoxContainer.add_child(item)
 	$VBoxContainer.move_child(item, 0)
 	
 	print('Day %d, %s [%s] %s' % [item.day, item.time, EventManager.EventType.find_key(item.type), item.content])
